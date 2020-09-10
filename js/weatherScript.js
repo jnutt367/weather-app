a1b876c35b49513cbd0feed36a49c6f11b221358
Jason Nutt <jnutt367@gmail.com>
2020-09-10 17:19:52 -0400

    Put files into folders
    Updated html to connect with file path for .js and .css.

---
+++ js/weatherScript.js
@@ -0,0 +1,28 @@
+
+const api = {
+	key: "c5a5b9545c8fdcc3e856b6a40d148783",
+	base: "https://api.openweathermap.org/data/2.5/"
+	}
+
+const searchbox = document.querySelector('.search-box');
+searchbox.addEventListener('keypress', setQuery);
+
+function setQuery(evt) {
+	if (evt.keyCode == 13) {
+		getResults(searchbox.value);
+		console.log(searchbox.value);
+	}
+}
+
+function getResults (query) {
+	fetch(`${api.base}weather?q=${query}weather?q=&imperial&APPID=${api.key}`)
+	.then(weather => {
+		return weather.json();
+	}).then(displayResults);
+}
+
+
+
+function displayResults (weather){
+	console.table(weather);
+}
